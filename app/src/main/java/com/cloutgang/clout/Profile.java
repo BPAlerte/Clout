0f1dc76511d0e0156336273a61e7e9c60fffc894
Jordan <jgarces97@gmail.com>
2019-02-17 00:11:41 -0500

    Connected the app to the Firebase

--- app/src/main/java/com/cloutgang/clout/Profile.java
+++ app/src/main/java/com/cloutgang/clout/Profile.java
@@ -1,16 +1,16 @@
 package com.cloutgang.clout;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Explode;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -33,9 +33,10 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_leaderboard);
+
     }
 
 }
