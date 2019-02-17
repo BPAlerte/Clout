f06299783a8ca952185387aa1cbfa732aed6c870
Jordan <jgarces97@gmail.com>
2019-02-16 21:16:50 -0500

    Got basic navigation bar features

--- app/src/main/java/com/cloutgang/clout/Profile.java
+++ app/src/main/java/com/cloutgang/clout/Profile.java
@@ -1,5 +1,6 @@
 package com.cloutgang.clout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -17,14 +18,10 @@ public class Profile extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_profile:
+                    startActivity(new Intent(Profile.this, MainActivity.class));
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_leaderboard:
                     return true;
             }
             return false;
